10e350d9c57c9773d07e237fe9f891388b3d9c7e
Sonnm90 <kenkon42@gmail.com>
2023-03-09 15:37:10 +0700

    up

--- bai3/MovablePoint.java
+++ bai3/MovablePoint.java
@@ -1,2 +1,57 @@
-package bai3;public class MovablePoint {
+package bai3;
+
+import java.util.Arrays;
+
+public class MovablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint() {
+    }
+
+    public float getXSpeed() {
+        return xSpeed;
+    }
+
+    public void setXSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setYSpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] arr = {xSpeed, ySpeed};
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{"
+                + Arrays.toString(getXY()) +",speed=" +Arrays.toString(getSpeed())+
+                '}';
+    }
+
+    public MovablePoint move(){
+        setX(getX()+xSpeed);
+        setY(getY()+ySpeed);
+      return this;
+
+    }
 }
--- bai3/Test.java
+++ bai3/Test.java
@@ -1,2 +1,18 @@
-package bai3;public class Test {
+package bai3;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        MovablePoint test= new MovablePoint(1,2,3,4);
+        System.out.println(Arrays.toString(test.getSpeed()));
+        System.out.println(test.getXSpeed());
+        System.out.println(test.getYSpeed());
+        System.out.println(test);
+
+        System.out.println(test.move());
+
+
+
+    }
 }

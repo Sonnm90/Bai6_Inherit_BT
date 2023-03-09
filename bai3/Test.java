b8e4ef059bc5c0f4730c81546f0af06c347af395
Sonnm90 <kenkon42@gmail.com>
2023-03-09 16:36:50 +0700

    up

--- bai3/Test.java
+++ bai3/Test.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        MovablePoint test= new MovablePoint(1,2,3,4);
+        MovablePoint test= new MovablePoint(1,1,3,4);
         System.out.println(Arrays.toString(test.getSpeed()));
         System.out.println(test.getXSpeed());
         System.out.println(test.getYSpeed());
-        System.out.println(test);
-
+        System.out.println(Arrays.toString(test.getXY()));
         System.out.println(test.move());
+        System.out.println(test.move());
+
 
 
 
--- bai4/Main.java
+++ bai4/Main.java
@@ -1,2 +1,23 @@
-package bai4;public class Main {
+package bai4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter value side 1: ");
+        triangle.setSide1(sc.nextDouble());
+        System.out.println("Enter value side 2: ");
+        triangle.setSide2(sc.nextDouble());
+        System.out.println("Enter value side 3: ");
+        triangle.setSide3(sc.nextDouble());
+
+        if (!triangle.checkTriangle()){
+            System.out.println("Not a triangle");
+        } else {
+            System.out.println(triangle);
+        }
+
+    }
 }
